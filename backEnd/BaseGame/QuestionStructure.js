8c6129242add3654730be8f0d51b039a9ffaaf40
LiamAnderberg <anderberg.liam@icloud.com>
2023-02-20 09:59:59 +0100

    Prototype Game for the terminal

---
+++ backEnd/BaseGame/QuestionStructure.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.Questions = void 0;
+exports.Questions = [
+    { question: "What is 1+1?", answer: "2", alternatives: ["4", ".5", "2", "1"] },
+    { question: "What is 1+2?", answer: "3", alternatives: ["3", "4", "1", "2"] }
+];
